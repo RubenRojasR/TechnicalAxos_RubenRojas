a4928d62458a20210e961291113c4aef5b55ca5c
RubenRojasR <rrnzdm@gmail.com>
2025-01-31 00:15:31 -0600

    Add project files.

---
+++ TechnicalAxos_RubenRojas/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TechnicalAxos_RubenRojas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
